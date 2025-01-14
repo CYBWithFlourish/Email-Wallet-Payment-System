bd08d32c7ec7a2774fdf1babc51699be31b9e1db
FLOURISH <137620937+CYBWithFlourish@users.noreply.github.com>
2025-01-14 23:55:49 +0100

    Update dev.nix

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,29 +1,31 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
+# configuring environment with nix, see: https://developers.google.com/idx/guides/customize-idx-env
+
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
   channel = "stable-24.05"; # or "unstable"
 
-  # Use https://search.nixos.org/packages to find packages
+#Use https://search.nixos.org/packages to find packages
+
   packages = [
      pkgs.go
      pkgs.rustc
-     pkgs.algolia-cli
      pkgs.anchor
      pkgs.solana-cli
      pkgs.solana-validator
-     pkgs.yarn
      pkgs.sudo
+     pkgs.algolia-cli
      pkgs.python311
      pkgs.python311Packages.pip
+     pkgs.yarn
      pkgs.nodejs_20
      pkgs.nodePackages.nodemon
   ];
 
-  # Sets environment variables in the workspace
+#Sets environment variables in the workspace
   env = {};
   idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+
+#Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       # "vscodevim.vim"
     ];
